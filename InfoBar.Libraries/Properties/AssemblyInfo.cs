e57659716b0f02af230e3eed166cd6a47419d992
Casper Sparre <Casper>
2013-01-16 00:42:00 +0100

    fixed spelling error on libraries project

--- InfoBar.Libraries/Properties/AssemblyInfo.cs
+++ InfoBar.Libraries/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("InfoBar.Libraies")]
-[assembly: AssemblyDescription("External libraies used in InfoBar.Modules")]
+[assembly: AssemblyTitle("InfoBar.Libraries")]
+[assembly: AssemblyDescription("External libraries used in InfoBar.Modules")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("KANT Development")]
 [assembly: AssemblyProduct("InfoBar")]
